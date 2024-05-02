65cebd0357ef07d779e0af1f1d255aeeffd4cf82
Nikola R. Hristov <nikola@playform.cloud>
2024-05-02 23:30:53 +0300

    squash!

--- lib/index.ts
+++
@@ -1,9 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
-export { download, downloadAndUnzipVSCode } from './download';
-export { runTests } from './runTest';
-export { resolveCliPathFromVSCodeExecutablePath, resolveCliArgsFromVSCodeExecutablePath } from './util';
-export * from './progress';
